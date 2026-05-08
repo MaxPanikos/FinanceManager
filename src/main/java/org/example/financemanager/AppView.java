32f65dc75dd4e99bf80921bd1f1d542691821ef1
OmegaSusik <maxpanik1@gmail.com>
2026-05-08 19:58:04 +0200

    changes

--- src/main/java/org/example/financemanager/AppView.java
+++ src/main/java/org/example/financemanager/AppView.java
@@ -1,12 +1,18 @@
 package org.example.financemanager;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 
 public class AppView extends StackPane {
     private Profile profile;
+
+    @FXML
+    private StackPane overlayPane, popupPane, contentPane;
+
     public AppView(Profile profile) {
         this.profile = profile;
 
@@ -18,5 +24,23 @@ public class AppView extends StackPane {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void showPopup (Pane pane) {
+        this.popupPane.getChildren().clear();
+        this.popupPane.getChildren().add(pane);
+        this.overlayPane.setVisible(true);
+        this.popupPane.setVisible(true);
+    }
+    @FXML
+    public void hidePopup () {
+        this.popupPane.getChildren().clear();
+        this.overlayPane.setVisible(false);
+        this.popupPane.setVisible(false);
+    }
+
+    public void setPage (Pane pane) {
+        this.contentPane.getChildren().clear();
+        this.contentPane.getChildren().add(pane);
     }
 }

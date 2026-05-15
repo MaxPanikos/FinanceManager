4900bb5602758d65d7a8a8abb0acafaa234a601d
OmegaSusik <maxpanik1@gmail.com>
2026-05-15 17:06:22 +0200

    upgraded user experience

--- src/main/java/org/example/financemanager/AddTransactionView.java
+++ src/main/java/org/example/financemanager/AddTransactionView.java
@@ -1,5 +1,6 @@
 package org.example.financemanager;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,6 +18,8 @@ import java.util.stream.Collectors;
 
 public class AddTransactionView extends VBox {
     private AppView appView;
+    private boolean userChoice;
+
     @FXML
     private Spinner<Double> amountSpinner;
     @FXML
@@ -33,13 +36,28 @@ public class AddTransactionView extends VBox {
         datePicker.setValue(LocalDate.now());
 
         amountSpinner.setEditable(true);
-        SpinnerValueFactory<Double> factory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 1_000_000_000.0, 0.0, 10.0);
+        SpinnerValueFactory<Double> factory = new SpinnerValueFactory.DoubleSpinnerValueFactory(-1_000_000_000.0, 1_000_000_000.0, 0.0, 10.0);
         amountSpinner.setValueFactory(factory);
 
         TextField editor = amountSpinner.getEditor();
         editor.setTextFormatter(new TextFormatter<>(change -> {
             String newText = change.getControlNewText();
             if (newText.matches("-?([0-9]*[\\.,]?[0-9]*)")) {
+                if (editor.getText().startsWith("-")) {
+                    Platform.runLater(() -> {
+                        typeSwitch.setSelected(true);
+                        typeSwitch.setDisable(true);
+                        this.userChoice = false;
+                    });
+                } else {
+                    if (!userChoice) {
+                        Platform.runLater(() -> {
+                            typeSwitch.setSelected(false);
+                            typeSwitch.setDisable(false);
+                        });
+                        this.userChoice = true;
+                    }
+                }
                 return change;
             }
             return null;
@@ -66,6 +84,7 @@ public class AddTransactionView extends VBox {
     public AddTransactionView(AppView appView) {
         super();
         this.appView = appView;
+        this.userChoice = true;
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("add-transaction-view.fxml"));
             fxmlLoader.setRoot(this);
@@ -95,6 +114,9 @@ public class AddTransactionView extends VBox {
     protected void addTx () {
         try {
             double amount = amountSpinner.getValue();
+            if (amount >= 0.0 && typeSwitch.isSelected()) {
+                amount = amount * -1;
+            }
             LocalDate date = datePicker.getValue();
             TransactionTypes type = typeComboBox.getValue();
 
